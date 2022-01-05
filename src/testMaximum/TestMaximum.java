51d2860882362002860fdf72230e676bb7591c8f
vinit <vinitsxn@gmail.com>
2022-01-05 11:27:21 +0530

    Added: Welcome message added

---
+++ src/testMaximum/TestMaximum.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: Vinit Kumar
+ * @created on :05-01-2022
+ * Check maximum among 3 values using Generics
+ */
+package testMaximum;
+
+public class TestMaximum {
+    //main method
+    public static void main(String[] args){
+    System.out.println("Compute maximum using Generics !!!");
+    }
+}
