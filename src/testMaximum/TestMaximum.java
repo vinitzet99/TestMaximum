efbdb903cf984cab0aa3b141cfd563fb633d089d
vinit <vinitsxn@gmail.com>
2022-01-05 11:36:24 +0530

    Added: Float Comparision Functionality added

--- src/testMaximum/TestMaximum.java
+++ src/testMaximum/TestMaximum.java
@@ -18,12 +18,27 @@ public class TestMaximum {
         System.out.printf("The maximum value among %s, %s, %s is %s.\n", x, y, z, max);
     }
 
+    //Find maximum value between 3 Float using CompareTo
+    public static void testMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if (max.compareTo(y) < 0) {
+            max = y;
+        }
+        if (max.compareTo(z) < 0) {
+            max = z;
+        }
+        System.out.printf("The maximum value among %s, %s, %s is %s.\n", x, y, z, max);
+    }
+
     //main method
     public static void main(String[] args) {
         System.out.println("Compute maximum using Generics !!!");
         //variable initialize
         Integer intX = 1, intY = 2, intZ = 3;
+        Float floatX = 1.1f, floatY = 4.1f, floatZ = 3.1f;
         //find maximum value
         testMaximum(intX, intY, intZ);
+        testMaximum(floatX, floatY, floatZ);
+
     }
 }
