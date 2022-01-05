a90bc15e9dd70add3cc0a821d3b43903df750f2e
vinit <vinitsxn@gmail.com>
2022-01-05 11:31:33 +0530

    Added: Integer Comparision Functionality added

--- src/testMaximum/TestMaximum.java
+++ src/testMaximum/TestMaximum.java
@@ -6,8 +6,24 @@
 package testMaximum;
 
 public class TestMaximum {
+    //Find maximum value between 3 Integer using CompareTo
+    public static void testMaximum(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (max.compareTo(y) < 0) {
+            max = y;
+        }
+        if (max.compareTo(z) < 0) {
+            max = z;
+        }
+        System.out.printf("The maximum value among %s, %s, %s is %s.\n", x, y, z, max);
+    }
+
     //main method
-    public static void main(String[] args){
-    System.out.println("Compute maximum using Generics !!!");
+    public static void main(String[] args) {
+        System.out.println("Compute maximum using Generics !!!");
+        //variable initialize
+        Integer intX = 1, intY = 2, intZ = 3;
+        //find maximum value
+        testMaximum(intX, intY, intZ);
     }
 }
