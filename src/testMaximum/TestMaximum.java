a827329a0fd7bf9e26aafe2ff599a82bab678fa5
vinit <vinitsxn@gmail.com>
2022-01-05 15:02:20 +0530

    Added: More then Three Parameters functionality added

--- src/testMaximum/TestMaximum.java
+++ src/testMaximum/TestMaximum.java
@@ -5,45 +5,50 @@
  */
 package testMaximum;
 
+import java.util.ArrayList;
+
 public class TestMaximum<T extends Comparable> {
     //generic instance variable
-    private T x, y, z;
+    private ArrayList<T> params=new ArrayList<T>();
 
     //generic constructor
-    public TestMaximum(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public TestMaximum(T... params) {
+        for(int i=0;i< params.length;i++){
+            this.params.add(params[i]);
+        }
     }
 
     /**
      * method to print maximum values
      * take input from testMaximum method
      */
-    public static <T> void printMax(T x,T y,T z,T max){
-        System.out.printf("The maximum value among %s, %s, %s is %s.\n",x,y,z,max);
+    public static <T> void printMax(T max){
+        System.out.printf("The maximum value is: %s\n",max);
     }
     /**
      * method to compute maximum
      * internally calls generic method testMaximum
      */
     public void maximum() {
-        TestMaximum.testMaximum(x, y, z);
+        TestMaximum.testMaximum(params);
     }
 
     /**
      * Generic method to compare 3 Strings/Integer/Float
      * Extends Comparable to compare values using CompareTo method
      */
-    public static <T extends Comparable> void testMaximum(T x, T y, T z) {
-        T max = x;
-        if (max.compareTo(y) < 0) {
-            max = y;
-        }
-        if (max.compareTo(z) < 0) {
-            max = z;
-        }
-        printMax(x,y,z,max); // calling print function
+    public static <T extends Comparable> void testMaximum(ArrayList<T> params) {
+        int n = params.size();
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (params.get(j).compareTo(params.get(j + 1))>0)
+                {
+                    // swap arr[j+1] and arr[j]
+                    T temp = params.get(j);
+                    params.set(j, params.get(j + 1));
+                    params.set(j + 1, temp);
+                }
+        printMax(params.get(n - 1)); // calling print function
     }
 
     //main method
