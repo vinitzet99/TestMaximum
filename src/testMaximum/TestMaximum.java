bae3f7f5d9f3528416caab71101596328442cf1f
vinit <vinitsxn@gmail.com>
2022-01-05 12:02:19 +0530

    Refactor: Changed TestMaximum class to Generic type

--- src/testMaximum/TestMaximum.java
+++ src/testMaximum/TestMaximum.java
@@ -5,31 +5,50 @@
  */
 package testMaximum;
 
-public class TestMaximum {
+public class TestMaximum<T extends Comparable> {
+    //generic instance variable
+    private T x, y, z;
+
+    //generic constructor
+    public TestMaximum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * method to compute maximum
+     * internally calls generic method testMaximum
+     */
+    public void maximum() {
+        TestMaximum.testMaximum(x, y, z);
+    }
+
     /**
      * Generic method to compare 3 Strings/Integer/Float
      * Extends Comparable to compare values using CompareTo method
      */
-    public static < T extends Comparable<T>> void testMaximum(T x,T y,T z){
-        T max=x;
-        if(max.compareTo(y)<0){
-            max=y;
+    public static <T extends Comparable> void testMaximum(T x, T y, T z) {
+        T max = x;
+        if (max.compareTo(y) < 0) {
+            max = y;
         }
-        if(max.compareTo(z)<0){
-            max=z;
+        if (max.compareTo(z) < 0) {
+            max = z;
         }
-        System.out.printf("The maximum value among %s, %s, %s is %s.\n",x,y,z,max);
+        System.out.printf("The maximum value among %s, %s, %s is %s.\n", x, y, z, max);
     }
+
     //main method
     public static void main(String[] args) {
         System.out.println("Compute maximum using Generics !!!");
         //variable initialize
         Integer intX = 1, intY = 2, intZ = 3;
         Float floatX = 1.1f, floatY = 4.1f, floatZ = 3.1f;
-        String stringX="Test3",stringY="Test2",stringZ="Test1";
+        String stringX = "Test3", stringY = "Test2", stringZ = "Test1";
         //find maximum value
-        testMaximum(intX, intY, intZ);
-        testMaximum(floatX, floatY, floatZ);
-        testMaximum(stringX,stringY,stringZ);
+        new TestMaximum(intX, intY, intZ).maximum();
+        new TestMaximum(floatX, floatY, floatZ).maximum();
+        new TestMaximum(stringX, stringY, stringZ).maximum();
     }
 }
